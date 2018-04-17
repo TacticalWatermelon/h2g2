2bb38817823b0ae33e6bd7767b754bdec7249daa
TacticalWatermelon <madscientist0101010@gmail.com>
2018-04-16 21:52:03 -0700

    Add files via upload

--- guide_search.py
+++ guide_search.py
@@ -13,27 +13,49 @@ Future phases will add support for voice input (for search), and computer voice 
 import wikipedia #imports wikipedia library
 import textwrap #used to format wiki text to improve readability
 from gtts import gTTS #importing gTTs module for text to speech conversion
-import os #imported to play converted audio
+import os #imported to play converted audiocommit,
 
 
 ############################################
 #Input block
 #guide_search = 'douglas adams' #static search string
-print("Welcome to Wikipedia")
+print("\n--------  DON'T PANIC  -------")
+print("Welcome to the Hitchhiker's Guide to the Galaxy!")
 print("options:\n(input text) --> search\n(quit)--> exits program")
 guide_search = input("Search the Guide for:   ")
 if guide_search == "quit":
     exit()
+print("Would you like the summary? Or the full entry?")
+report = input()
 ############################################
+# looking up guide entry summary
 ############################################
-# looking up wiki entry specified by input string
+if report == "summary":
+    print("got it: summary\n")
+    summ_para = wikipedia.summary(guide_search)
+    summ_para_split = str.splitlines(summ_para)
+    for lines in range(0, len(summ_para_split)):
+        line = textwrap.fill(summ_para_split[lines], initial_indent = '    ')
+        print(line)
 ############################################
-summ_para = wikipedia.summary(guide_search)
-summ_para_split = str.splitlines(summ_para)
-for lines in range(0, len(summ_para_split)):
-    line = textwrap.fill(summ_para_split[lines], initial_indent = '    ')
-    print(line)
-print("\n\n Please wait for audio\n") #reminds user to wait for audio
+# looking up guide entry full text
+############################################
+elif report == "full":
+    print("got it: full entry\n")
+    full_entry = wikipedia.page(guide_search)
+    full_entry_split = str.splitlines(full_entry)
+    for lines in range(0,len(full_entry_split)):
+        line = textwrap.fill(full_entry_split[lines], initial_indent = '    ')
+        print(line)
+
+else:
+    print("Sorry, i don't understand. Please reinitialize and try again.")
+    exit()
+
+print("\n\n Please wait for audio\n")  # reminds user to wait for audio
+############################################
+
+
 ############################################
 '''Temporarily commented out to improve speed of testing
 ############################################
@@ -42,8 +64,8 @@ print("\n\n Please wait for audio\n") #reminds user to wait for audio
 # Language in which you want to convert
 language = 'en'
 
-# Passing the text and language to the engine,
-# here we have marked slow=False. Which tells
+# Passing the text and language to th   e engine,
+# here we have marked slow=False. Whi   ch tells
 # the module that the converted audio should
 # have a high speed
 myobj = gTTS(text = summ_para, lang = language, slow = False)
